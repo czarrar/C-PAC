705f192a893b6f4883f47001d8452dd8a3c883e7
Zarrar Shehzad <czarrar@gmail.com>
2013-10-23 11:58:47 -0400

    fixes to datasource to get certain variables

--- CPAC/pipeline/datasource.py
+++ CPAC/pipeline/datasource.py
@@ -1,7 +1,24 @@
 import nipype.pipeline.engine as pe
 import nipype.interfaces.utility as util
 from CPAC.utils.datasource import create_func_datasource
-from CPAC.pipeline.tests.cpac_pipeline import create_log_node
+from CPAC.utils.utils import create_log
+
+def create_log_node(wflow, output, indx, log_dir, workflow, scan_id = None):
+    #call logging workflow
+
+    if wflow: 
+        log_wf = create_log(wf_name = 'log_%s' %wflow.name)
+        log_wf.inputs.inputspec.workflow = wflow.name
+        log_wf.inputs.inputspec.index = indx
+        log_wf.inputs.inputspec.log_dir = log_dir
+        workflow.connect(wflow, output, log_wf, 'inputspec.inputs')
+    else:
+        log_wf = create_log(wf_name = 'log_done_%s'%scan_id, scan_id= scan_id)
+        log_wf.inputs.inputspec.workflow = 'DONE'
+        log_wf.inputs.inputspec.index = indx
+        log_wf.inputs.inputspec.log_dir = log_dir
+        log_wf.inputs.inputspec.inputs = log_dir
+        return log_wf
 
 def create_anat_datasource(path, wf_name='anat_datasource'):
     import nipype.pipeline.engine as pe
@@ -24,7 +41,7 @@ def create_anat_datasource(path, wf_name='anat_datasource'):
     
     return wf
 
-def add_anat_resource(name, path, subject_id, strat, num_strat, extra=False):
+def add_anat_resource(name, path, subject_id, strat, num_strat, log_dir, workflow, extra=False):
     """
     Generates a datasource for an existing anatomical file
     and adds it to the list of strategies.
@@ -33,16 +50,16 @@ def add_anat_resource(name, path, subject_id, strat, num_strat, extra=False):
     """
     anatFlow = create_anat_datasource(path, '%s_gather_%d' % (name, num_strat))
     anatFlow.inputs.inputnode.subject = subject_id
-    strat.update_resource_pool({key:(anatFlow, 'outputspec.anat')})
+    strat.update_resource_pool({name:(anatFlow, 'outputspec.anat')})
     
     if extra:
         strat.append_name(anatFlow.name)
         strat.set_leaf_properties(anatFlow, 'outputspec.anat')
-        create_log_node(anatFlow, 'outputspec.anat', num_strat)
+        create_log_node(anatFlow, 'outputspec.anat', num_strat, log_dir, workflow)
 
     return strat
 
-def add_func_resource(name, paths, subject_id, strat, num_strat, extra=False):
+def add_func_resource(name, paths, subject_id, strat, num_strat, log_dir, workflow, extra=False):
     """
     Generates a datasource for an existing set of functional files
     and adds it to the list of strategies.
@@ -63,8 +80,8 @@ def add_func_resource(name, paths, subject_id, strat, num_strat, extra=False):
     
     strat.append_name(funcFlow.name)
     strat.set_leaf_properties(funcFlow, 'outputspec.rest')
-    strat.update_resource_pool({key:(funcFlow, 'outputspec.rest')})
+    strat.update_resource_pool({name:(funcFlow, 'outputspec.rest')})
     
-    create_log_node(funcFlow, 'outputspec.rest', num_strat)
+    create_log_node(funcFlow, 'outputspec.rest', num_strat, log_dir, workflow)
     
     return strat
